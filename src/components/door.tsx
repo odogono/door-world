5ad44b662f3157a800ffcb7a0dc8378088e3b0b2
Alex Veenendaal <alex@opendoorgonorth.com>
2025-03-19 11:40:24 +0000

    chore: click area slightly larger, apply colors

--- src/components/door.tsx
+++ src/components/door.tsx
@@ -2,12 +2,15 @@ import { createLog } from '@helpers/log';
 import { useGLTF } from '@react-three/drei';
 import { useFrame } from '@react-three/fiber';
 import { useEffect, useRef, useState } from 'react';
-import { Group, Object3D } from 'three';
+import { Object3D } from 'three';
+import { applyColor } from '../helpers/object-3d';
 
 interface DoorProps {
   position?: [number, number, number];
   scale?: [number, number, number];
   rotation?: [number, number, number];
+  frameColor?: string;
+  doorColor?: string;
 }
 
 const log = createLog('Door');
@@ -17,35 +20,44 @@ const ROTATION_THRESHOLD = 0.01; // Threshold to determine if animation is compl
 export const Door = ({
   position = [0, 0.5, 0],
   scale = [1, 1, 1],
-  rotation = [0, -Math.PI / 2, 0]
+  rotation = [0, -Math.PI / 2, 0],
+  doorColor = '#83D5FF',
+  frameColor = '#FFF'
 }: DoorProps) => {
   const { scene } = useGLTF('/vbasic.door.glb');
   const [isOpen, setIsOpen] = useState(false);
   const doorRef = useRef<Object3D>(null);
+  const frameRef = useRef<Object3D>(null);
   const targetRotation = isOpen ? Math.PI / 2 : 0;
   const [isAnimating, setIsAnimating] = useState(false);
 
-  // log.debug('scene', scene);
-  // log.debug('nodes', nodes);
-  // log.debug('materials', materials);
-
   useEffect(() => {
-    // Find the door node and store its reference
+    // Find the door and frame nodes and store their references
     const doorNode = scene.getObjectByName('door');
+    const frameNode = scene.getObjectByName('door-frame');
+
+    if (!doorNode || !frameNode) {
+      log.error('Door or frame node not found');
+      return;
+    }
+
     if (doorNode) {
       doorRef.current = doorNode;
-      log.debug('Found door node', doorNode);
-    } else {
-      log.debug('No door node found in scene');
+      applyColor(doorNode, doorColor);
+    }
+
+    if (frameNode) {
+      frameRef.current = frameNode;
+      applyColor(frameNode, frameColor);
     }
 
     // Cleanup function to dispose of the model when component unmounts
     return () => {
       useGLTF.preload('/vbasic.door.glb');
     };
-  }, [scene]);
+  }, [scene, frameColor]);
 
-  useFrame((state, delta) => {
+  useFrame((_state, delta) => {
     if (!doorRef.current || !isAnimating) return;
 
     // Smoothly interpolate the rotation
@@ -69,13 +81,18 @@ export const Door = ({
   };
 
   return (
-    <primitive
-      object={scene}
-      position={position}
-      scale={scale}
-      rotation={rotation}
-      onClick={handleClick}
-    />
+    <>
+      <primitive
+        object={scene}
+        position={position}
+        scale={scale}
+        rotation={rotation}
+      />
+      <mesh position={position} onClick={handleClick} visible={false}>
+        <boxGeometry args={[0.8, 1, 0.4]} />
+        <meshBasicMaterial color="red" />
+      </mesh>
+    </>
   );
 };
 
