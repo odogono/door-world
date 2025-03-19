697bcc034f2c791bcf4c9aeb56814a57aaafeb2b
Alex Veenendaal <alex@opendoorgonorth.com>
2025-03-19 22:09:06 +0000

    chore: door size

--- src/model/dungeon/constants.ts
+++ src/model/dungeon/constants.ts
@@ -7,6 +7,6 @@ export const ROOM_SIZE_LARGE: RoomSizeRange = [80, 120];
 export const NUM_ROOMS_PER_CLICK = 3;
 export const MAX_ROOMS = 500; // Maximum number of rooms in the dungeon
 export const DOOR_WIDTH = 8;
-export const DOOR_HEIGHT = 1;
+export const DOOR_HEIGHT = 8;
 
 export const CANVAS_SIZE = 1000; // Size of the canvas for dungeon generation
